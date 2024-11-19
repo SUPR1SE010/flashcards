b0e4fe517a2c5c5b246b8a5e39a9f29775890606
SUPR1SE010 <robarbosa6195385@gmail.com>
2024-11-19 11:30:12 -0300

    Até aqui estava tudo funcionando daqui pra frente eu não me responsabilizo

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital do acre?',
+    'A capital do acre é Rio Branco'
+)
+
+criaCartao(
+    'Anime',
+    'Quantos pokemons tem na região de kanto',
+    'A região de kanto tem 151 pokemons de acordo com a pokedex'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Qual é a moeda mais valorizada do mundo?',
+    'A moeda mais valorizada do mundo é o dinar do kwait'
+)
+
+criaCartao(
+    'aleatorio',
+    'Como alcançar a paz mundial?',
+    'Tudo depende da flexibilidade do rabo da largatixa'
+)
